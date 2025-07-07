ff9057566ffd60f0d3282b73c830a2c8007f862f
Domagoj Latečki <domagoj.latecki@gmail.com>
2025-07-07 19:09:45 +0200

    display relative working dirs

--- functions.sh
+++ functions.sh
@@ -12,35 +12,37 @@ U() {
 }
 
 sw() {
-    local TARGET=""
+    # TODO: track OLDPWD
+    local target=""
 
     if [ -z "$TW_ACTIVE" ]; then
-        if [ -z "$OTHER_PWD_SET" ]; then
-            OTHER_PWD_SET="t"
+        if [ -z "$OTHER_PWD_IDX" ]; then
+            OTHER_PWD_IDX="1"
             OTHER_PWD="$PWD"
             OTHER_OLD_PWD="$OLDPWD"
         fi
 
-        local ACTIVE_DIR_CLR="$PS1_DIR_CLR"
+        if [ "$OTHER_PWD_IDX" -eq "1" ]; then
+            OTHER_PWD_IDX="2"
+        else 
+            OTHER_PWD_IDX="1"
+        fi
 
-        PS1_DIR_CLR="$PS1_ALT_DIR_CLR"
-        PS1_ALT_DIR_CLR="$ACTIVE_DIR_CLR"
-
-        TARGET="$OTHER_PWD"
+        target="$OTHER_PWD"
 
         OTHER_PWD="$PWD"
     else
         TW_ACTIVE=""
-        TARGET="$NTW_PWD"
+        target="$NTW_PWD"
         NTW_PWD=""
         NTW_OLD_PWD=""
     fi
 
     if [ ! -z "$1" ]; then
-        TARGET="$1"
+        target="$1"
     fi
 
-    cd "$TARGET"
+    cd "$target"
 }
 
 #syn/sw/Swaps current working directory.
@@ -62,9 +64,7 @@ qw() {
     TW_ACTIVE=""
     NTW_PWD=""
     NTW_OLD_PWD=""
-    PS1_DIR_CLR="01;33m"
-    PS1_ALT_DIR_CLR="01;34m"
-    OTHER_PWD_SET=""
+    OTHER_PWD_IDX=""
     OTHER_PWD="$PWD"
 }
 
@@ -73,38 +73,47 @@ qw() {
 #doc/qw/Unsets the alternate working directory set by the sw alias.
 
 sw_colored_wd() {
-    local WD_CLR="\[\033[$PS1_DIR_CLR\]"
-    local A_CLR="\[\033[01;36m\]"
-    local RST_CLR="\[\033[0m\]"
-    local ANCHOR_SYMBOL=" ∫ "
+    local wdClr="\[\033[$PS1_DIR_CLR\]"
+    local oWdClr="\[\033[$PS1_ALT_DIR_INACTIVE_CLR\]"
+
+    if [ "${OTHER_PWD_IDX:-0}" -eq "2" ]; then
+        wdClr="\[\033[$PS1_ALT_DIR_CLR\]"
+        oWdClr="\[\033[$PS1_DIR_INACTIVE_CLR\]"
+    fi
+
+    local aClr="\[\033[$ANCHOR_CLR\]"
+    local rstClr="\[\033[0m\]"
  
-    local ANCHORED_WD="${WD_CLR}\w${RST_CLR}"
+    local anchoredWd="${wdClr}\w${rstClr}"
 
     if [[ -n "$ANCHOR" ]]; then
         if [[ "$PWD" == "$ANCHOR/"* ]]; then
-            local BEFORE_ANCHOR="$ANCHOR"
-            local AFTER_ANCHOR="${PWD/$ANCHOR\//}"
+            local beforeAnchor="$ANCHOR"
+            local afterAnchor="${PWD/$ANCHOR\//}"
 
-            local ANCHORED="${BEFORE_ANCHOR}${RST_CLR}${A_CLR}${ANCHOR_SYMBOL}${RST_CLR}${WD_CLR}${AFTER_ANCHOR}${RST_CLR}"
-            ANCHORED_WD="${WD_CLR}${ANCHORED/$HOME/~}"
+            local anchored="${beforeAnchor}${rstClr}${aClr}${ANCHOR_SYMBOL}${rstClr}${wdClr}${afterAnchor}${rstClr}"
+            anchoredWd="${wdClr}${anchored/$HOME/~}"
         elif [[ "$ANCHOR" == "/" && "$PWD" != "/" ]]; then
-            ANCHORED_WD="${PWD/\//${A_CLR}$ANCHOR_SYMBOL${RST_CLR}${WD_CLR}}${RST_CLR}"
+            anchoredWd="${PWD/\//${aClr}$ANCHOR_SYMBOL${rstClr}${wdClr}}${rstClr}"
         else
-            ANCHORED_WD="${WD_CLR}${PWD/$HOME/~}${RST_CLR}"
+            anchoredWd="${wdClr}${PWD/$HOME/~}${rstClr}"
         fi
     fi
 
     if [ -n "$TW_ACTIVE" ]; then
-        echo -n "[\[\033[$PS1_TW_DIR_CLR\]\w\[\033[0m\]]"
-    elif [ -n "$OTHER_PWD_SET" ]; then
-        if [ "$PWD" != "$OTHER_PWD" ]; then
-            local symbol="*"
-        else
-            local symbol="."
+        echo -n "[\[\033[$PS1_TW_DIR_CLR\]\w$rstClr]"
+    elif [ -n "$OTHER_PWD_IDX" ]; then
+        local relWdPath=$(realpath -s --relative-to="$PWD" "$OTHER_PWD")
+
+        # TODO: add dot prefix when OTHER_PWD is in current dir
+
+        if [ "$OTHER_PWD_IDX" -eq "1" ]; then
+            echo -n "[$anchoredWd|$oWdClr$relWdPath$rstClr]"
+        else 
+            echo -n "[$oWdClr$relWdPath$rstClr|$anchoredWd]"
         fi
-        echo -n "[\[\033[$PS1_ALT_DIR_CLR\]$symbol\[\033[0m\]|$ANCHORED_WD]"
     else
-        echo -n "[$ANCHORED_WD]"
+        echo -n "[$anchoredWd]"
     fi
 }
 
@@ -139,12 +148,12 @@ y() {
             echo "Selected file(s): $Y_FILES, source directory: $Y_SRC"
             read -p "Continue (y/n)? " answer
             if [ "$answer" = "y"  ]; then
-                local TMP_PWD="$PWD"
-                local TMP_OLDPWD="$OLDPWD"
+                local tmpPwd="$PWD"
+                local tmpOldPwd="$OLDPWD"
                 cd "$Y_SRC"
-                cp -t "$TMP_PWD" $Y_FILES
-                cd "$TMP_PWD"
-                OLDPWD="$TMP_OLDPWD"
+                cp -t "$tmpPwd" $Y_FILES
+                cd "$tmpPwd"
+                OLDPWD="$tmpOldPwd"
             else
                 echo "Canceled."
             fi
@@ -160,7 +169,6 @@ y() {
 #doc/y/Selects files to be copied. When no files are specified, previously selected files are copied into current working directory.
 
 current_java_version() {
-    local ACTIVE_JAVA_VERSION_CLR="01;36m"
     echo -ne "[\[\033[$ACTIVE_JAVA_VERSION_CLR\]\xE2\x98\x95$ACTIVE_JAVA_VERSION\[\033[0m\]]"
 }
 
@@ -172,9 +180,9 @@ jv() {
             echo "No such Java version: $1"
         else
             ACTIVE_JAVA_VERSION="$1"
-            WITHOUT_JAVA_PATH="${PATH/${JAVA_HOME}\/bin:/}"
+            local withoutJavaPath="${PATH/${JAVA_HOME}\/bin:/}"
             export JAVA_HOME="${JAVA_VERSIONS[$ACTIVE_JAVA_VERSION]}"
-            export PATH="${JAVA_HOME}/bin:${WITHOUT_JAVA_PATH}"
+            export PATH="${JAVA_HOME}/bin:${withoutJavaPath}"
             echo "Switched to Java version $ACTIVE_JAVA_VERSION"
         fi
     fi
@@ -184,27 +192,26 @@ jv() {
 #doc/jv/Usage: jv [version]
 
 opt_newline_and_exit_code() {
-    local EC=$?
+    local ec=$?
+    local ecClr="$EXIT_CODE_Z_CLR"
 
-    if [ $EC -ne 0 ]; then
-        local EE_CLR="01;31m"
-    else
-        local EE_CLR="01;37m"
+    if [ $ec -ne 0 ]; then
+        local ecClr="$EXIT_CODE_NZ_CLR"
     fi
 
-    IFS=';' read -sdR -p $'\E[6n' ROW COL
+    IFS=';' read -sdR -p $'\E[6n' row col
 
-    if [ $COL -ne 1 ]; then
-        echo -n "\[\033[0;31m\]⊖ ↵\[\033[0m\]\n\[\033[0m\]╭─[\u@\h][\[\033[$EE_CLR\]$EC\[\033[0m\]]"
+    if [ $col -ne 1 ]; then
+        echo -n "\[\033[0;31m\]⊖ ↵\[\033[0m\]\n\[\033[0m\]╭─[\u@\h][\[\033[$ecClr\]$ec\[\033[0m\]]"
     else
-        echo -n "\[\033[0m\]╭─[\u@\h][\[\033[$EE_CLR\]$EC\[\033[0m\]]"
+        echo -n "\[\033[0m\]╭─[\u@\h][\[\033[$ecClr\]$ec\[\033[0m\]]"
     fi
 }
 
 bg_job_count() {
-    local NUM_JOBS="$(jobs | wc -l)"
+    local numJobs="$(jobs | wc -l)"
 
-    if [ $NUM_JOBS -ne 0 ]; then
-        echo -n "{\[\033[01;35m\]λ$NUM_JOBS\[\033[0m\]}"
+    if [ $numJobs -ne 0 ]; then
+        echo -n "{\[\033[$JOBS_CLR\]λ$numJobs\[\033[0m\]}"
     fi
 }
